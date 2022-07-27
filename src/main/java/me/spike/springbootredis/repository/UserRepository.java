17720634ffefd7890af69af013c79cacd3b37d56
Ajay <ajaydi@thoughtworks.com>
2022-07-27 08:49:56 +0100

    Remove unnecessary serializable interface

--- src/main/java/me/spike/springbootredis/repository/UserRepository.java
+++ src/main/java/me/spike/springbootredis/repository/UserRepository.java
@@ -11,20 +11,20 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class UserRepository {
-    private final RedisTemplate<String, UserDto> redisTemplate;
+    private final RedisTemplate<String, UserDbo> redisTemplate;
 
-    public UserRepository(RedisTemplate<String, UserDto> redisTemplate) {
+    public UserRepository(RedisTemplate<String, UserDbo> redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     @Transactional
     public void save(CreateUserRequest newUser, String id) {
-        redisTemplate.opsForValue().set(id, new UserDto(newUser.getName(), id));
+        redisTemplate.opsForValue().set(id, new UserDbo(newUser.getName(), id));
         redisTemplate.expire(id, 30, TimeUnit.SECONDS);
     }
 
     public Optional<User> find(String id) {
-        final UserDto userDto = redisTemplate.opsForValue().get(id);
-        return userDto == null ? Optional.empty() : Optional.of(userDto.toModel());
+        var user = redisTemplate.opsForValue().get(id);
+        return user == null ? Optional.empty() : Optional.of(user.toModel());
     }
 }
